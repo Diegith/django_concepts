8a912f2da5b255c780f038068f4189c2ed6c639b
Diegith <diego.albarracinacero>
2024-11-17 16:53:55 -0500

    vistas como Clases

--- firts_project/first_app/views.py
+++ firts_project/first_app/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-def my_view(request):
-    car_list =[
-        {
-            'title': 'Toyota', 
-            'title': 'BMW'
-         },
-    ]
-    context = {
-        'car_list': car_list
-    }
+class CarListView(TemplateView):
+    template_name = "first_app/car_list.html"
 
-    return render(request, "first_app/car_list.html", context)
+    def get_context_data(self):
+        car_list =[
+            {'title': 'Ferrari'},
+            {'title': 'Toyota'}, 
+            {'title': 'BMW'}         
+        ]        
+        return {
+            'car_list': car_list
+        }
+
+def my_test_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
